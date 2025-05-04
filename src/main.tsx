dc9507d4f1ed2cba3e2baed319662ac54807a5a8
desinsight <ceo@desinsight.com>
2025-05-04 12:43:54 +0900

    fix: base href 및 BrowserRouter basename을 '/snap-codex-scheduler'로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ async function initializeMockServiceWorker() {
 initializeMockServiceWorker().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/snap-codex-scheduler">
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
